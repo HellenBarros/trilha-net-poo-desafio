2dc5798aa2dbc297f748903038325446f82dc92c
Hellen Barros <hellenbarrosac@gmail.com>
2024-01-13 18:22:50 -0300

    Desafio bootcamp Decola Tech Concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,22 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
+        public string Numero { get; set; }
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+
+        public string Modelo{get {return _modelo;} set {_modelo = value;}}
+        public string Imei{get {return _imei;} set {_imei = value;}}
+        public int Memoria{get {return _memoria;} set {_memoria = value;}}        
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Teste Iphone
+Console.WriteLine("Teste da classe iphone");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo iphone", imei: "111111111", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsapp");
+
+
+//Teste Nokia
+Console.WriteLine("Teste da classe Nokia");
+Smartphone nokia = new Nokia(numero: "789012", modelo: "Modelo nokia", imei: "000000000", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
